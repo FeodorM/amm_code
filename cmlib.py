e9af1d9268a3ba847ce6e1d3ed64969c493e10c2
FeodorM <Feodor.Mann@gmail.com>
2016-04-18 22:35:04 +0300

    cm added

--- cmlib.py
+++
@@ -1,9 +0,0 @@
-from matplotlib.pylab import table, show
-
-
-def showMatr(matrix):
-    table(cellText=matrix,
-          loc='center',
-          cellLoc='center',
-          bbox=[0.2, 0.2, 0.5, 0.5])
-    show()
