04552f24979cbcdd4e26a65132497a213e35e61e
FeodorM <Feodor.Mann@gmail.com>
2016-04-25 19:30:50 +0300

    implement linsolve method (метод прогонки), fix Vector (inheritance is better than previous implementation)

--- TridiagonalSLE/sle.py
+++ TridiagonalSLE/sle.py
@@ -22,24 +22,29 @@ class TridiagonalMatrix:
         return TridiagonalMatrix(*[Vector.random(size) for _ in range(3)])
 
     @staticmethod
-    def from_file(file_name, as_matrix=False):
+    def from_file(file, as_matrix=False):
         """
         Read tridiagonal matrix from file.
         If as_matrix is Fasle,
         read 3 lines and interprete them as a, b, c vectors.
         Oterwise, read whole matrix, but then take just a, b, c from it.
         """
-        with open(file_name) as file:
-            if as_matrix:
-                matrix = [[float(x) for x in line.split()] for line in file]
-                return TridiagonalMatrix(
-                    [0] + [matrix[i + 1][i] for i in range(len(matrix) - 1)],
-                    [matrix[i][i] for i in range(len(matrix))],
-                    [matrix[i][i + 1] for i in range(len(matrix) - 1)] + [0]
-                )
-            else:
-                return TridiagonalMatrix(*[Vector.from_file(file)
-                                            for _ in range(3)])
+        if as_matrix:
+            matrix = [[float(x) for x in line.split()] for line in file]
+            return TridiagonalMatrix(
+                [0] + [matrix[i + 1][i] for i in range(len(matrix) - 1)],
+                [matrix[i][i] for i in range(len(matrix))],
+                [matrix[i][i + 1] for i in range(len(matrix) - 1)] + [0]
+            )
+        else:
+            return TridiagonalMatrix(*[Vector.from_file(file)
+                                        for _ in range(3)])
+
+    def __repr__(self):
+        return "TridiagonalMatrix({self.a}\n{i}{self.b}\n{i}{self.c}".format(
+            i="                  ",
+            self=self
+        )
 
     def __str__(self):
         return '\n'.join(
@@ -100,7 +105,7 @@ class TridiagonalMatrix:
                 [self.a[i] * other[i - 1] +
                  self.b[i] * other[i] +
                  self.c[i] * other[i + 1]
-                    for i in range(1, len(x) - 1)] +
+                    for i in range(1, len(other) - 1)] +
                 [self.a[-1] * other[-2] + self.b[-1] * other[-1]]
             )
         raise TypeError(
@@ -112,119 +117,30 @@ class TridiagonalMatrix:
         Solve SLE $Ax = vector$
         return x
         """
-        pass
+        assert len(self) == len(vector), "Vector and matrix should have same size"
+        ls = [0]
+        ms = [0]
+        for i, a, b, c, d in zip(range(len(self)),
+                self.a, self.b, self.c, vector):
+            ms.append((d - a * ms[-1]) / (b - a * ls[-1]))
+            ls.append(c / (b - a * ls[-1]))
+        ls.pop()
+        ls.pop(0)
+        ms.pop(0)
+        x = [ms.pop()]
+        for l, m in zip(reversed(ls), reversed(ms)):
+            x.append(m - l * x[-1])
+        return Vector(reversed(x))
 
 
-class TridiagonalMatrix_:
-    """
-    TridiagonalMatrix --- matrix with three diagonals,
-    every other element is zero
-
-    """
-    def __init__(self, a, b, c):
-        a[0] = 0
-        c[-1] = 0
-        self.lines = zip(a, b, c)
-
-    def __len__(self):
-        return len(self.lines)
-
-    @staticmethod
-    def random(size):
-        """
-        Return random tridiagonal matrix of given size
-        """
-        return TridiagonalMatrix_(*[Vector.random(size) for _ in range(3)])
-
-    @staticmethod
-    def from_file(file_name, as_matrix=False):
-        """
-        Read tridiagonal matrix from file.
-        If as_matrix is Fasle,
-        read 3 lines and interprete them as a, b, c vectors.
-        Oterwise, read whole matrix, but then take just a, b, c from it.
-        """
-        with open(file_name) as file:
-            if as_matrix:
-                matrix = [[float(x) for x in line.split()] for line in file]
-                return TridiagonalMatrix(
-                    [0] + [matrix[i + 1][i] for i in range(len(matrix) - 1)],
-                    [matrix[i][i] for i in range(len(matrix))],
-                    [matrix[i][i + 1] for i in range(len(matrix) - 1)] + [0]
-                )
-            else:
-                return TridiagonalMatrix(*[Vector.from_file(file)
-                                            for _ in range(3)])
-
-    def __str__(self):
-        return '\n'.join(
-            '\t'.join(str(round(x, 3)) for x in line)
-                for line in self.matrix()
-        )
-
-    @staticmethod
-    def _zeros(n):
-        return [0] * n
-
-    def matrix(self):
-        """
-        Return representation of self as a matrix
-        """
-        size = len(self) - 2
-        zeros = self._zeros
-        middle = [
-            zeros(i - 1) + [a, b, c] + zeros(size - i)
-                for i, (a, b, c) in enumerate(
-                    zip(self.a[1:], self.b[1:], self.c[1:-1]), 1
-                )
-        ]
-        return (
-            [self.b[:1] + self.c[0:1] + [0] * (len(self) - 2)] +
-            middle +
-            [[0] * (len(self) - 2) + self.a[-1:] + self.b[-1:]]
-        )
-
-    def __add__(self, other):
-        assert len(self) == len(other), "Matrices should have same size"
-        return TridiagonalMatrix(
-            [x + y for x, y in zip(self.a, other.a)],
-            [x + y for x, y in zip(self.b, other.b)],
-            [x + y for x, y in zip(self.c, other.c)]
-        )
-
-    def __neg__(self):
-        return TridiagonalMatrix(
-            [-x for x in self.a],
-            [-x for x in self.b],
-            [-x for x in self.c]
-        )
-
-    def __sub__(self, other):
-        return self + (-other)
-
-    def __mul__(self, other):
-        """
-        Matrix product with tridiagonal matrix or vector
-        """
-        assert len(self) == len(other), "Matrices should have same size"
-        if isinstance(other, TridiagonalMatrix):
-            pass
-        elif isinstance(other, Vector):
-            return Vector(
-                [self.lines[0][1] * other[0] + self.lines[0][2] * other[1]] +
-                [a * other[i - 1] +
-                 b * other[i] +
-                 c * other[i + 1]
-                    for (a, b, c), i in zip(self.lines[1:-1], range(1, len(self) - 1))] +
-                [self.a[-1] * other[-2] + self.b[-1] * other[-1]]
-            )
-        raise TypeError(
-            "Wring type for multiplication: {}".format(other.__class__)
-        )
-
-    def linsolve(self, vector):
-        """
-        Solve SLE $Ax = vector$
-        return x
-        """
-        pass
+if __name__ == '__main__':
+    # with open('test') as file:
+    #     m = TridiagonalMatrix.from_file(file)
+    #     v = Vector.from_file(file)
+    m = TridiagonalMatrix.random(100)
+    x = Vector.random(100)
+    d = m * x
+    solution = m.linsolve(d)
+    # print(solution)
+    # print(x)
+    print(round(abs(x - solution), 10))
--- TridiagonalSLE/vector.py
+++ TridiagonalSLE/vector.py
@@ -1,12 +1,7 @@
 from random import random
 
-class Vector:
+class Vector(list):
     """Just mathematical vector"""
-    def __init__(self, list_):
-        self.list = list(list_)
-
-	def __len__(self):
-        return len(self.list)
 
     @staticmethod
     def random(size):
@@ -19,29 +14,31 @@ class Vector:
         return Vector(float(x) for x in file.readline().split())
 
     def __repr__(self):
-        return "Vector({})".format(
-            ', '.join(str(x) for x in self.list))
+        return "Vector({})".format(list.__repr__(self))
 
     def __str__(self):
-        return '\t'.join(str(x) for x in self.list)
-
-    def __getitem__(self, index):
-        return self.list[index]
-
-    def __setitem__(self, index, value):
-        self.list[index] = value
+        return '[ ' + '  '.join(str(round(x, 2)) for x in self) + ' ]'
 
     def __add__(self, other):
         assert len(self) == len(other), "Vectors should have same size"
-        return Vector(x + y for x, y in zip(self.list, other.list))
+        return Vector(x + y for x, y in zip(self, other))
 
     def __sub__(self, other):
         return self + (-other)
 
     def __neg__(self):
-        return Vector(-x for x in self.list)
+        return Vector(-x for x in self)
 
     def dot(self, other):
         """Dot (scalar) product"""
         assert len(self) == len(other), "Vectors should have same size"
-        return sum(x * y for x, y in zip(self.list, other.list))
+        return sum(x * y for x, y in zip(self, other))
+
+    def __abs__(self):
+        return max(abs(x) for x in self)
+
+    norm = __abs__
+
+if __name__ == '__main__':
+    v = Vector.random(10)
+    print(repr(v))
