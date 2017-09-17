782afaed45d20d225496d762a3038d904bc7a505
FeodorM <Feodor.Mann@gmail.com>
2017-09-17 23:49:11 +0300

    add it-security/cipher.py

---
+++ it-security/cipher.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+def chiper(string):
+    return ''.join(cipher_dict[x] for x in string)
+
+def dechipher(string):
+    return ''.join(decipher_dict[x] for x in string)
+
+
+cipher_dict = dict(zip("abcdefghijklmnopqrstuvwxyz ", " abcdefghijklmnopqrstuvwxyz"))
+decipher_dict = {v: k for k, v in cipher_dict.items()}
+
+s = argv[1]
+
+ciphered = chiper(s)
+dechiphered = dechipher(ciphered)
+print('input = "{}"'.format(s))
+print('chiphered = "{}"'.format(ciphered))
+print('dechiphered = "{}"'.format(dechiphered))
