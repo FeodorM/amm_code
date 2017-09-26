ee78b3fa06de0b8c5dd046e03ccfe0fa36a6a7c2
FeodorM <Feodor.Mann@gmail.com>
2017-09-26 22:16:05 +0300

    add docs to utils

--- numerical-analysis/utils/utils.py
+++ numerical-analysis/utils/utils.py
@@ -1,12 +1,25 @@
 from math import *
-from typing import Callable
+from typing import Callable, List
 
 
-# TODO: docs
-def parse(expr: str, args) -> Callable[[float, float, float], float]:
+def parse(expr: str, args: str) -> Callable[[float, float, float], float]:
+    """
+    Parse function from string
+    :param expr: string, containing function expression. Just expression.
+        It can contain fny variable from args and any math function (from Python's math module)
+    :param args: list of variables in form of "x, y, z"
+    :return: anonymous function -- lambda args: expr
+    """
     return eval("lambda {}: {}".format(args, expr))
 
 
-def create_xs(a, b, n):
+def create_xs(a: float, b: float, n: int) -> List[float]:
+    """
+    Create mesh with step h = (b - a) / n and boundaries a and b.
+    :param a: left boundary
+    :param b: right boundary
+    :param n: number of subintervals
+    :return: list of floats
+    """
     h = (b - a) / n
     return [a + i * h for i in range(n + 1)]
