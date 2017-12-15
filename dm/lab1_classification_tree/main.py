e0f8b00a36ec7f9dbd5899d66f46cf76250c3d17
FManukovskiy <fmanukovskiy@vrn.beeline.ru>
2017-12-15 17:30:02 +0300

    smth with dm

--- dm/lab1_classification_tree/main.py
+++ dm/lab1_classification_tree/main.py
@@ -8,6 +8,14 @@ from sklearn import tree
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 from sklearn import preprocessing
+import pydot
+
+import numpy as np
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.metrics import classification_report, confusion_matrix
 
 df = pd.read_csv("potreb.csv", header=0)#, parse_dates=[1])
 le = preprocessing.LabelEncoder()
@@ -25,41 +33,63 @@ le.fit(values_to_encode)
 for cat in categories:
     df[cat] = le.transform(df[cat])
 
+# sclearn gridsearch
+
 target = df.loc[:, expirations]
 predictors = df.loc[:, features[0]:features[-1]]
-X_train, X_test, y_train, y_test = train_test_split(predictors
-    , target
-    , test_size=0.4
-    , random_state=42
-)
-clf = tree.DecisionTreeClassifier(
-    random_state=17,
-    max_depth=3,
-    min_samples_leaf=10,
-    class_weight='balanced',
-    max_features=0.8
-)
-
-clf = clf.fit(X_train, y_train)
-scores = cross_val_score(clf, X_test, y_test, cv=5, scoring='f1')
-print(scores)
-print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-ind = list(range(1000))
-value = [list(predictors.loc[x]) for x in ind]
-tar = [target.loc[x] for x in ind]
-# [[2, 36, 2, 5, 2384, 1, 2, 4, 3, 1, 1, 4, 33, 3, 1, 1, 2, 1, 1, 1],
-        #  [4, 24, 2, 3, 1533, 1, 2, 4, 2, 1, 3, 3, 38, 2, 2, 1, 3, 1, 2, 1]]
-pred = list(clf.predict(value))
-
-res = sum(1 if t == p else 0 for t, p in zip(tar, pred))
-print(res, len(ind))
-
-
-# dot_data = tree.export_graphviz(clf, feature_names=['laufkont', 'laufzeit', 'moral', 'verw', 'hoehe',
-#                                                     'sparkont', 'beszeit', 'rate', 'famges', 'buerge', 'wohnzeit',
-#                                                     'verm', 'alter', 'weitkred', 'wohn', 'bishkred', 'beruf',
-#                                                     'pers', 'telef', 'gastarb'],
-#                                 class_names=['credit', 'no_credit'],
-#                                 out_file='small_tree.dot', filled=True)
+# X_train, X_test, y_train, y_test = train_test_split(predictors
+#     , target
+#     , test_size=0.4
+#     , random_state=42
+# )
+# clf = tree.DecisionTreeClassifier(
+#     random_state=17,
+#     min_samples_leaf=10,
+#     class_weight='balanced',
+#     max_features=0.8
+# )
+# clf = clf.fit(X_train, y_train)
+# scores = cross_val_score(clf, X_test, y_test, cv=5, scoring='f1')
+# print(scores)
+# print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+#
+# dot_data = tree.export_graphviz(
+#     clf,
+#     feature_names=features,
+#     class_names=['Просрочка', 'нет'],
+#     out_file='small_tree.dot',
+#     filled=True
+# )
 # (graph,) = pydot.graph_from_dot_file('small_tree.dot')
-# graph.write_png('example1_graph.png')
+# graph.write_png('example_graph.png')
+# select p value | select k best
+# features = SelectKBest(chi2 , k=14).fit_transform(predictors, target)
+predictors = predictors.drop(code, axis=1)
+print(target.value_counts())
+print(
+    pd.DataFrame(np.c_[(predictors.columns, *chi2(predictors, target))],
+    columns=['var', 'chi2', 'p']
+    ).sort_values('p').head(20))
+features = SelectKBest(chi2 , k=5).fit_transform(predictors, target)
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.7, random_state=100)
+# sample_weight_last_ten = abs(pd.np.random.randn(len(X_train)))
+# sample_weight_last_ten[5:] *= 5
+# sample_weight_last_ten[9] *= 15
+clf4 = tree.DecisionTreeClassifier(class_weight=[4, 1])
+tree_para = {
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [4, 5, 6, 7, 8, 9, 10, 11, 12, 15],
+    # 'min_samples_split': [4, 10, 15, 20, 30, 40, 50, 90, 120, 150],
+    'splitter': ['best', 'random'],
+    'class_weight': ['balanced', {0: 1, 1: 4}, {0: 1, 1: 10}]
+ }
+clf4 = GridSearchCV(clf4, tree_para, cv=5, scoring=['neg_log_loss', 'roc_auc'], refit='neg_log_loss')
+clf4.fit(X_train, y_train)
+print(clf4.best_params_)
+y_pred = clf4.predict(X_test)
+print(classification_report(y_test, y_pred))
+scores = cross_val_score(clf4, X_test, y_test, cv=2, scoring='f1')
+print(confusion_matrix(y_test, y_pred))
+
+# print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+# print()
